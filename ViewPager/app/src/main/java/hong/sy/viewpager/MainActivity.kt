199bc6ccb0efb45eaf34904fbf30d175b61a4404
seoy9 <ghfdwe@gmail.com>
2022-09-19 15:47:11 +0900

    ViewPager 기초

---
+++ ViewPager/app/src/main/java/hong/sy/viewpager/MainActivity.kt
@@ -0,0 +1,100 @@
+package hong.sy.viewpager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.viewpager2.widget.ViewPager2
+import hong.sy.viewpager.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    // 전역 변수로 바인딩 객체 선언
+    private var mBinding: ActivityMainBinding? = null
+    // 매번 null 체크를 할 필요 없이 편의성을 위해 바인딩 변수 재 선언
+    private val binding get() = mBinding!!
+
+    private val MIN_SCALE = 0.85f // 뷰가 몇퍼센트로 줄어들 것인지
+    private val MIN_ALPHA = 0.5f // 어두워지는 정도를 나타낸 듯 하다.
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // 기존 setContentView 를 제거해주시고..
+        //setContentView(R.layout.activity_main)
+
+        // 자동 생성된 뷰 바인딩 클래스에서의 inflate라는 메서드를 활용해서
+        // 액티비티에서 사용할 바인딩 클래스의 인스턴스 생성
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+
+        // getRoot 메서드로 레이아웃 내부의 최상위 위치 뷰의
+        // 인스턴스를 활용하여 생성된 뷰를 액티비티에 표시 합니다.
+        setContentView(binding.root)
+
+        // 이제부터 binding 바인딩 변수를 활용하여 마음 껏 xml 파일 내의 뷰 id 접근이 가능해집니다.
+
+        /* 여백, 너비에 대한 정의 */
+        val pageMarginPx = resources.getDimensionPixelOffset(R.dimen.pageMargin) // dimen 파일 안에 크기를 정의해두었다.
+        val pagerWidth = resources.getDimensionPixelOffset(R.dimen.pageWidth) // dimen 파일이 없으면 생성해야함
+        val screenWidth = resources.displayMetrics.widthPixels // 스마트폰의 너비 길이를 가져옴
+        val offsetPx = screenWidth - pageMarginPx - pagerWidth
+
+        binding.viewPagerIdol.setPageTransformer { page, position ->
+            page.translationX = position * -offsetPx
+        }
+
+        binding.viewPagerIdol.offscreenPageLimit = 1 // 몇 개의 페이지를 미리 로드 해둘것인지
+        binding.viewPagerIdol.adapter = ViewPagerAdapter(getIdolList()) // 어댑터 생성
+        binding.viewPagerIdol.orientation = ViewPager2.ORIENTATION_HORIZONTAL // 방향을 가로로
+        // binding.viewPagerIdol.setPageTransformer(ZoomOutPageTransformer()) // 애니메이션 적용
+        // 애니메이션과 여백은 함께 설정할 수 없음
+    }
+
+    // 뷰 페이저에 들어갈 아이템
+    private fun getIdolList(): ArrayList<Int> {
+        return arrayListOf<Int>(R.drawable.idol1, R.drawable.idol2, R.drawable.idol3)
+    }
+
+    // 액티비티가 파괴될 때..
+    override fun onDestroy() {
+        // onDestroy 에서 binding class 인스턴스 참조를 정리해주어야 한다.
+        mBinding = null
+        super.onDestroy()
+    }
+
+    /* 공식문서에 있는 코드 긁어온거임 */
+    inner class ZoomOutPageTransformer : ViewPager2.PageTransformer {
+        override fun transformPage(view: View, position: Float) {
+            view.apply {
+                val pageWidth = width
+                val pageHeight = height
+                when {
+                    position < -1 -> { // [-Infinity,-1)
+                        // This page is way off-screen to the left.
+                        alpha = 0f
+                    }
+                    position <= 1 -> { // [-1,1]
+                        // Modify the default slide transition to shrink the page as well
+                        val scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position))
+                        val vertMargin = pageHeight * (1 - scaleFactor) / 2
+                        val horzMargin = pageWidth * (1 - scaleFactor) / 2
+                        translationX = if (position < 0) {
+                            horzMargin - vertMargin / 2
+                        } else {
+                            horzMargin + vertMargin / 2
+                        }
+
+                        // Scale the page down (between MIN_SCALE and 1)
+                        scaleX = scaleFactor
+                        scaleY = scaleFactor
+
+                        // Fade the page relative to its size.
+                        alpha = (MIN_ALPHA +
+                                (((scaleFactor - MIN_SCALE) / (1 - MIN_SCALE)) * (1 - MIN_ALPHA)))
+                    }
+                    else -> { // (1,+Infinity]
+                        // This page is way off-screen to the right.
+                        alpha = 0f
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ViewPager/app/src/main/java/hong/sy/viewpager/ViewPagerAdapter.kt
@@ -0,0 +1,25 @@
+package hong.sy.viewpager
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import hong.sy.viewpager.databinding.IdolListItemBinding
+
+class ViewPagerAdapter(idolList: ArrayList<Int>) : RecyclerView.Adapter<ViewPagerAdapter.PagerViewHolder>() {
+    var item = idolList
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = PagerViewHolder((parent))
+
+    override fun getItemCount(): Int = item.size
+
+    override fun onBindViewHolder(holder: PagerViewHolder, position: Int) {
+        holder.idol.setImageResource(item[position])
+    }
+
+    inner class PagerViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder
+        (LayoutInflater.from(parent.context).inflate(R.layout.idol_list_item, parent, false)){
+
+        val idol : ImageView = itemView.findViewById(R.id.imageView_idol)
+    }
+}
